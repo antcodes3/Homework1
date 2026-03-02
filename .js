bf1cf2075eb6136a5092ea64a2bb000501e0b45f
antcodes3 <anewsome188@gmail.com>
2026-03-02 16:07:23 -0600

    Implement date display and range slider in .js
    Added dynamic date display and range slider functionality.

--- .js
+++ .js
@@ -1,1 +1,18 @@
-.js
+/*
+    NAME: Anthony Newsome
+    Date created:
+    Date Modified:
+    Purpose: Homework 1 
+*/
+
+//dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+// range slider js code
+let slider = document.getElementById("range");
+    let output = document.getElementById("range-slider");
+    output.innerHTML = slider.value;
+
+slider.oninput = function () {output.innerHTML = this.value;};
